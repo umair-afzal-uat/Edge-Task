45f36f4910860d78f76d890205db1d6c9ee4abb7
Umair Afzal <128166400+umair-afzal-uat@users.noreply.github.com>
2024-08-08 21:55:20 +0500

    fix: design fixations for production
    Design fixations for production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -974,16 +974,16 @@
             </div>
             @if (session()->has('account_id'))
             <nav class="-mx-3 flex flex-1 justify-end">
-                <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-white  ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                     Dashboard
                 </a>
             </nav>
             @else
             <nav class="-mx-3 flex flex-1 justify-end">
-                <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                     Log in
                 </a>
-                <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                <a href="{{ route('register') }}" class="rounded-md px-3 py-2  text-white  ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                     Register
                 </a>
             </nav>
